f9b23c5d66890a64ceafa75cf35a1b15e243a1da
mattBtindall <matthewbtindall@gmail.com>
2022-06-29 09:31:51 +0100

    Add basis of project, create local server, create module that holds the get sql products, send result to frontend

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-'use strict';
+const http = require('http');
 
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    const products = require('./get-products').myProducts.then(products => {
+        res.end(JSON.stringify(products));
+    });
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
---
+++ get-products.js
@@ -0,0 +1,17 @@
+const Api = require('opencart-sql-api');
+const Products = require('opencart-sql-api/src/repository/Products');
+
+const myStoreDB = new Api({
+  database: {
+    connection: {
+      host: 'localhost',
+      port: 3306,
+      user: 'opencart',
+      password: 'opencart',
+      database: 'opencart',
+    }
+  },
+});
+  
+const productsRepository = new Products(myStoreDB);  
+exports.myProducts = productsRepository.list({ page: 1, limit: 5 });
